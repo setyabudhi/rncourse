616b9605511e89bbc2c22096051e1613a0da5782
taufiq.setyabudhi <muhammad.setyabudhi@dieboldnixdorf.com>
2019-07-18 16:13:03 +0700

    add static image

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from "react-native";
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
+import placeImage from "./src/assets/beautiful-place.jpg";
 
 export default class App extends Component {
   state = {
@@ -12,16 +13,20 @@ export default class App extends Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random() + '',
+          name: placeName,
+          image : placeImage
+        })
       };
     });
   };
 
-  placeDeleteHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place,i) => {
-            return i != index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
     });
@@ -31,8 +36,10 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} 
-                  onItemDeleted={this.placeDeleteHandler}/>
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
   }
--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import React from "react";
+import { StyleSheet, FlatList } from "react-native";
 
-import ListItem from '../ListItem/ListItem';
+import ListItem from "../ListItem/ListItem";
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} 
-                  placeName={place} 
-                  onItemPressed={() => props.onItemDeleted(i)}
-                  />
-      ));
-    return (
-        <ScrollView style={styles.listContainer}>{placesOutput}</ScrollView>
-    );
+  return (
+    <FlatList
+      style={styles.listContainer}
+      data={props.places}
+      renderItem={(info) => (
+        <ListItem
+          placeName={info.item.name}
+          placeImage={info.item.image}
+          onItemPressed={() => props.onItemDeleted(info.item.key)}
+        />
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    listContainer: {
-      width: "100%"
-    }
+  listContainer: {
+    width: "100%"
+  }
 });
 
 export default placeList;
